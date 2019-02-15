2a56ff34a9a16d2d4f3c8fcbb88c4aa571fac306
Rayhanshuvo <rayhanshuvo.bd@gmail.com>
2019-02-16 01:21:15 +0600

    set sum

---
+++ src/problem/SumOfFirstNTerms.java
@@ -0,0 +1,65 @@
+package problem;
+
+public class SumOfFirstNTerms {
+
+	//1+2+3+4+5+6+7+8+9+10
+	/*
+	 * public static void main(String[] args) { // TODO Auto-generated method stub
+	 * int array[]= {1,2,3,4,5,6,7,8,9,10}; int number=10; int storingVarOfArr=0;
+	 * for(int i=0;i<10;i++) {
+	 * 
+	 * storingVarOfArr=storingVarOfArr+array[i];
+	 * 
+	 * 
+	 * } System.err.println(storingVarOfArr);
+	 * 
+	 * }
+	 */
+	
+	//1+3+5+7+9
+	/*
+	 * public static void main(String[] args) { // TODO Auto-generated method stub
+	 * int array[]= {1,2,3,4,5,6,7,8,9,10}; int number=10; int storingVarOfArr=0;
+	 * for(int i=0;i<10;i++) {
+	 * 
+	 * if(array[i]%2==1) { storingVarOfArr=storingVarOfArr+array[i]; } else {
+	 * 
+	 * System.err.println("Not Odd Value"+array[i]); }
+	 * 
+	 * 
+	 * } System.err.println(storingVarOfArr);
+	 * 
+	 * }
+	 */
+	
+//2+4+6+8+10	
+	/*
+	 * public static void main(String[] args) { // TODO Auto-generated method stub
+	 * int array[]= {1,2,3,4,5,6,7,8,9,10}; int number=10; int storingVarOfArr=0;
+	 * for(int i=0;i<10;i++) {
+	 * 
+	 * if(array[i]%2==0) { storingVarOfArr=storingVarOfArr+array[i]; } else {
+	 * 
+	 * System.err.println(" Odd Value"+array[i]); }
+	 * 
+	 * 
+	 * } System.err.println(storingVarOfArr);
+	 * 
+	 * }
+	 */
+	/* 1/1+1/2+1/n , n=10*/
+	/*
+	 * public static void main(String[] args) { // TODO Auto-generated method stub
+	 * double array[]= {1,2,3,4,5,6,7,8,9,10}; double number=10; double
+	 * storingVarOfArr=0; for(int i=0;i<10;i++) {
+	 * 
+	 * storingVarOfArr=storingVarOfArr+1/array[i];
+	 * 
+	 * 
+	 * } System.out.println(storingVarOfArr);
+	 * 
+	 * 
+	 * }
+	 */
+
+}
